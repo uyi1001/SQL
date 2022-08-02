b76bee0e746d6165ed194ca64a9e8f96208ac8a5
Uyi <91153600+uyi1001@users.noreply.github.com>
2022-08-01 23:10:16 -0400

    Update in class content_MySQL.sql

--- in class content_MySQL.sql
+++ in class content_MySQL.sql
@@ -587,7 +587,18 @@ from (
 
 
 
-
+-- subqueries in from clause
+-- must give the derived table an alias
+-- can use view instead (better)
+select max(difference) as max_difference, min(difference) as min_difference
+from (
+	select client_id, name,
+		(select sum(invoice_total) from invoices
+			where client_id = c.client_id) as total_sales,
+		(select avg(invoice_total) from invoices) as average,
+		(select total_sales - average) as difference
+	from clients c
+) as sales_summary;
 
 
 
