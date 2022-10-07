0a891dfef2ce1771bab40f2a740b959ea5b7a683
Uyi <91153600+uyi1001@users.noreply.github.com>
2022-10-06 23:20:23 -0400

    Update HackerRank Practice_MySQL.sql

--- HackerRank Practice_MySQL.sql
+++ HackerRank Practice_MySQL.sql
@@ -65,4 +65,11 @@ group by occupation
 order by count(occupation), occupation;
 
 
+/******
+Pivot the Occupation column in OCCUPATIONS so that each Name is sorted alphabetically and displayed underneath its corresponding Occupation. The output column headers should be Doctor, Professor, Singer, and Actor, respectively.
 
+Note: Print NULL when there are no more names corresponding to an occupation.
+
+Input Format
+
+The OCCUPATIONS table is described as follows:
