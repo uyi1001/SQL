043631a26c7ec713314451ceb51b77dd1100731a
Uyi <91153600+uyi1001@users.noreply.github.com>
2022-11-11 23:46:48 -0500

    Update in class content_MySQL.sql

--- in class content_MySQL.sql
+++ in class content_MySQL.sql
@@ -681,4 +681,22 @@ join order_items
 using (product_id)
 group by product_id;
 
-
+-- case function
+select
+	order_id,
+    case
+		when year(order_date) = '2018' then 'Active'
+        when year(order_date) = '2018' - 1 then 'Last year'
+        when year(order_date) < '2018' - 1 then 'Archived'
+        else 'Future'
+	end as category
+from orders;
+select
+	concat(first_name, ' ', last_name),
+	points,
+    case
+		when points > 3000 then 'Gold'
+        when points >= 2000 then 'Silver'
+        else 'Bronze'
+	end as category
+from customers;
